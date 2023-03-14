687e5cfed17947c6d20e6e42d6595daa3b972af2
Sergei_Kuznetsov <SKuznetsov@asist-lab.com>
2023-03-14 14:21:21 +0300

    commit

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,22 +1,34 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/data/user';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent {
+  /*   roles: string[] = ['Гость', 'Модератор', 'Администратор']
+  model: User = new User(1, '', '') */
 
-  roles: string[] = ['Гость', 'Модератор', 'Администратор']
-  model: User = new User(1, '', '')
+  /*
+  Каждое поле формы инициализируем следующим образом:
+*/
+  userForm = new FormGroup({
+    /*
+  Внутри FormControl первым аргументом можем указать дефолтные значения поля;
+  Вторым аргументом - валидаторы
 
-  showNameModel(d: any) {
-    console.log(d)
+*/
+    name: new FormControl('User', Validators.required),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(7),
+    ]),
+  });
+
+  onSubmit(): void {
+    console.log(this.userForm);
+    console.log(this.userForm.value);
   }
-
-  onSubmit(d: any): void {
-    console.log(d)
-  }
-
 }
