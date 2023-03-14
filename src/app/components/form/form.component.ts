f003eac979ee19499d5d727c98ff497852b8cdc3
Sergei_Kuznetsov <SKuznetsov@asist-lab.com>
2023-03-14 10:04:16 +0300

    commit

---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { User } from 'src/app/data/user';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+
+  roles: string[] = ['Гость', 'Модератор', 'Администратор']
+  model: User = new User(1, '', '')
+
+
+
+
+}
---
+++ src/app/data/user.ts
@@ -0,0 +1,13 @@
+export class User {
+  constructor(
+    public id: number,
+    public name: string,
+    public role: string,
+    public age?: string,
+
+
+  ) {}
+
+
+
+}
